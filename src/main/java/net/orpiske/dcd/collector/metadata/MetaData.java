917d7090db8e5a8a623644fe677f42bc16dad33c
Otavio Piske <angusyoung@gmail.com>
2014-02-18 18:23:07 -0300

    Added comments

--- src/main/java/net/orpiske/dcd/collector/metadata/MetaData.java
+++ src/main/java/net/orpiske/dcd/collector/metadata/MetaData.java
@@ -17,26 +17,51 @@ package net.orpiske.dcd.collector.metadata;
 
 import net.orpiske.dcd.collector.vocabulary.Word;
 
+/**
+ * A simple metadata representation
+ */
 public class MetaData {
     private Word word;
     private int occurrences = 0;
 
+    /**
+     * Constructor
+     * @param word the word tied to the metadata
+     */
     public MetaData(final Word word) {
         this.word = word;
     }
 
+
+    /**
+     * Gets the word
+     * @return the word object
+     */
     public Word getWord() {
         return word;
     }
 
+    /**
+     * Sets the word object
+     * @param word the word object
+     */
     public void setWord(Word word) {
         this.word = word;
     }
 
+
+    /**
+     * Gets the number of occurrences of 'word' in the processed data set
+     * @return the number of occurrences
+     */
     public int getOccurrences() {
         return occurrences;
     }
 
+
+    /**
+     * Adds an occurrence of 'word'
+     */
     public void addOccurrence() {
         occurrences++;
     }
