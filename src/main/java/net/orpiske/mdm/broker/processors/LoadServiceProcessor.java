332effa7e3e271d0432eae9d7ae198babbf9d7ae
Otavio Piske <angusyoung@gmail.com>
2014-02-22 13:32:02 -0300

    Code cleanups, improved error handling and comments

--- src/main/java/net/orpiske/mdm/broker/processors/LoadServiceProcessor.java
+++ src/main/java/net/orpiske/mdm/broker/processors/LoadServiceProcessor.java
@@ -18,35 +18,50 @@ package net.orpiske.mdm.broker.processors;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.cxf.message.MessageContentsList;
+import org.apache.log4j.Logger;
 
 import javax.xml.ws.Holder;
 
+/**
+ * A processor for the load service
+ */
 public class LoadServiceProcessor implements Processor {
+    private static final Logger logger = Logger.getLogger(LoadServiceProcessor.class);
+    private static final int SUCCESS = 0;
+    private static final String SUCCESS_MSG = "Success";
+
     @Override
     public void process(Exchange exchange) throws Exception {
-        System.out.println("Message received");
-
+        logger.debug("Processing a load service message");
         Object object = exchange.getIn().getBody();
 
         if (object instanceof MessageContentsList) {
             MessageContentsList messageContentsList = (MessageContentsList) object;
 
-            Object input = messageContentsList.get(0);
-
-            System.out.println("Type: " + input.getClass());
-
+            /*
+             * We don't really do any processing here. We just want to
+             * acknowledge that we received the message
+             */
 
             Holder<Integer> code = (Holder<Integer>) messageContentsList.get(4);
             Holder<String> message = (Holder<String>) messageContentsList.get(5);
 
-            code.value = 0;
-            message.value = "Success";
+            code.value = SUCCESS;
+            message.value = SUCCESS_MSG;
 
-            System.out.println("Setting up reply message");
-
+            logger.debug("Setting up reply message");
             exchange.getOut().setBody(new Object[] { code, message });
         }
-
-
+        else {
+            /*
+             * A CXF in POJO modo should give us a MessageContentsList object.
+             * If that's not the case, then it's an unhandled error.
+             *
+             * Obs.: this is not a production-ready code since it should return
+             * _some_ error and does not.
+             */
+            logger.error("Unhandled message type: " + (object == null ? "null"
+                    : object.getClass()));
+        }
     }
 }
