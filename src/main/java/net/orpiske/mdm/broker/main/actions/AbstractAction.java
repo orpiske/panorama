fb3cc78cf49b77b7c4bac2ea863b6e222d689efb
Otavio Piske <angusyoung@gmail.com>
2014-02-25 14:53:11 -0300

    Fixed help text

--- src/main/java/net/orpiske/mdm/broker/main/actions/AbstractAction.java
+++ src/main/java/net/orpiske/mdm/broker/main/actions/AbstractAction.java
@@ -35,7 +35,7 @@ public abstract class AbstractAction {
 	protected void help(final Options options, int code) {
 		HelpFormatter formatter = new HelpFormatter();
 
-		formatter.printHelp("dcd", options);
+		formatter.printHelp("mdm-broker", options);
 		System.exit(code);
 	}
 
