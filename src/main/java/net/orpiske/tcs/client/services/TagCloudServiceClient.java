b678605763c285d3dc6f63e30033c5070c303f8e
Otavio R. Piske <angusyoung@gmail.com>
2014-05-04 14:02:56 -0300

    Added support for authenticating with the backend

--- src/main/java/net/orpiske/tcs/client/services/TagCloudServiceClient.java
+++ src/main/java/net/orpiske/tcs/client/services/TagCloudServiceClient.java
@@ -16,20 +16,26 @@
 
 package net.orpiske.tcs.client.services;
 
-import com.google.common.base.CharMatcher;
 import net.orpiske.tcs.client.base.EndPoint;
 import net.orpiske.tcs.client.base.ServiceClient;
+import net.orpiske.tcs.client.exceptions.InvalidCredentialsException;
 import net.orpiske.tcs.service.core.domain.Domain;
 import net.orpiske.tcs.service.core.domain.DomainList;
-import net.orpiske.tcs.service.core.domain.Tag;
 import net.orpiske.tcs.service.core.domain.TagCloud;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
+import org.springframework.http.*;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
+// import org.apache.http.client.HttpClient;
+
 public class TagCloudServiceClient implements ServiceClient {
+    private static final Logger logger = Logger.getLogger(TagCloudServiceClient.class);
 
     private EndPoint endPoint;
     private RestTemplate restTemplate;
@@ -41,13 +47,54 @@ public class TagCloudServiceClient implements ServiceClient {
 
         List<HttpMessageConverter<?>> objects = restTemplate.getMessageConverters();
         objects.add(new MappingJackson2HttpMessageConverter());
+
         restTemplate.setMessageConverters(objects);
     }
 
+
+    private HttpHeaders getHeaders() {
+        String user = System.getenv("TCS_USER");
+        String password = System.getenv("TCS_PASSWORD");
+
+        if (user == null || user.isEmpty()) {
+            logger.fatal("The backend system username is not provided (please set " +
+                    "the TCS_USER environment variable)");
+
+            throw new InvalidCredentialsException("The backend system username is not provided");
+        }
+
+        if (password == null || password.isEmpty()) {
+            logger.fatal("The backend system password is not provided (please set " +
+                    "the TCS_PASSWORD environment variable)");
+
+            throw new InvalidCredentialsException("The backend system password is not provided");
+        }
+
+        String auth = user + ":" + password;
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes());
+        headers.add("Authorization", "Basic " + new String(encodedAuth));
+
+        return headers;
+    }
+
+
     @Override
     public TagCloud requestTagCloud() {
-        TagCloud tagCloud = restTemplate.getForObject(endPoint.getTagCloudServiceLocation(),
-                TagCloud.class);
+
+        ResponseEntity<TagCloud> responseEntity =
+                restTemplate.exchange(
+                        endPoint.getTagCloudServiceLocation(),
+                        HttpMethod.GET,
+                        new HttpEntity<Object>(getHeaders()),
+                        TagCloud.class);
+
+        TagCloud tagCloud = responseEntity.getBody();
 
         return tagCloud;
     }
@@ -55,23 +102,29 @@ public class TagCloudServiceClient implements ServiceClient {
 
     @Override
     public TagCloud requestTagCloud(final Domain domain) {
-        System.out.println("Trying to request data for domain " + domain.getDomain());
-        System.out.println(" with name " + domain.getName());
-        System.out.println(" at location: "
-                + endPoint.getTagCloudServiceLocationForDomain());
+        if (logger.isDebugEnabled()) {
+            logger.debug("Trying to request data for domain " + domain.getDomain());
+            logger.debug(" with name " + domain.getName());
+            logger.debug(" at location: "
+                    + endPoint.getTagCloudServiceLocationForDomain());
+        }
 
 
-        TagCloud tagCloud = restTemplate.postForObject(
+        return restTemplate.postForObject(
                     endPoint.getTagCloudServiceLocationForDomain(),
                 domain,
                 TagCloud.class);
-
-        return tagCloud;
     }
 
     @Override
     public DomainList requestDomainList() {
-        return restTemplate.getForObject(endPoint.getDomainListLocation(),
-                DomainList.class);
+        ResponseEntity<DomainList> responseEntity =
+                restTemplate.exchange(
+                        endPoint.getDomainListLocation(),
+                        HttpMethod.GET,
+                        new HttpEntity<Object>(getHeaders()),
+                        DomainList.class);
+
+        return responseEntity.getBody();
     }
 }
